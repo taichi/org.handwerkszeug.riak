4ab470c530799555e3171816664764a56cc7c77a
taichi <ryushi@gmail.com>
2011-04-17 22:47:55 +0900

    add method to MapReduceResponse.

--- src/org/handwerkszeug/riak/op/mapreduce/MapReduceResponse.java
+++ src/org/handwerkszeug/riak/op/mapreduce/MapReduceResponse.java
@@ -1,5 +1,14 @@
 package org.handwerkszeug.riak.op.mapreduce;
 
+/**
+ * @author taichi
+ */
 public interface MapReduceResponse {
 
+	/**
+	 * @return phase no maybe null
+	 */
+	Integer getPhase();
+
+	byte[] getResponse();
 }
