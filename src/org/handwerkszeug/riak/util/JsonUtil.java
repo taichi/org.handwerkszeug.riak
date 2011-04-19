c19f12e691880fd8fc61d3c44930bdc9417bd59f
taichi <ryushi@gmail.com>
2011-04-19 17:20:07 +0900

    beginning of unit test.

---
+++ src/org/handwerkszeug/riak/util/JsonUtil.java
@@ -0,0 +1,58 @@
+package org.handwerkszeug.riak.util;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.handwerkszeug.riak.util.Streams.IORuntimeException;
+
+/**
+ * @author taichi
+ */
+public class JsonUtil {
+
+	public static JsonNode read(InputStream in) {
+		try {
+			ObjectMapper om = new ObjectMapper();
+			return om.readTree(new BufferedInputStream(in));
+		} catch (IOException e) {
+			throw new IORuntimeException(e);
+		} finally {
+			Streams.close(in);
+		}
+	}
+
+	public static String getJsonPath(Class<?> clazz, String name) {
+		StringBuilder stb = new StringBuilder();
+		stb.append(clazz.getName().replace('.', '/'));
+		stb.append("_");
+		stb.append(name);
+		stb.append(".json");
+		return stb.toString();
+	}
+
+	public static JsonNode read(final Class<?> clazz, final String name) {
+		final JsonNode[] result = new JsonNode[1];
+		new Streams.using<InputStream, IOException>() {
+			@Override
+			public InputStream open() throws IOException {
+				ClassLoader cl = clazz.getClassLoader();
+				return cl.getResourceAsStream(getJsonPath(clazz, name));
+			}
+
+			@Override
+			public void handle(InputStream stream) throws IOException {
+				result[0] = read(stream);
+			}
+
+			@Override
+			public void happen(IOException exception) {
+				throw new IORuntimeException(exception);
+			}
+		};
+		return result[0];
+	}
+
+}
--- test/org/handwerkszeug/riak/mapreduce/AbstractMapReduceQueryTest.java
+++ test/org/handwerkszeug/riak/mapreduce/AbstractMapReduceQueryTest.java
@@ -1,12 +1,20 @@
 package org.handwerkszeug.riak.mapreduce;
 
+import static org.junit.Assert.assertEquals;
+
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.node.ObjectNode;
+import org.handwerkszeug.riak.util.JsonUtil;
 import org.junit.Before;
 import org.junit.Test;
 
 public class AbstractMapReduceQueryTest {
 
 	AbstractMapReduceQuery target;
+
+	static JsonNode read(String name) {
+		return JsonUtil.read(AbstractMapReduceQueryTest.class, name);
+	}
 
 	@Before
 	public void setUp() throws Exception {
@@ -16,9 +24,11 @@ public class AbstractMapReduceQueryTest {
 
 	@Test
 	public void testPrepareBucket() throws Exception {
-		this.target.setInputs("testbucket");
+		this.target.setInputs("testPrepareBucket");
 		ObjectNode act = this.target.prepare();
-		System.out.println(act);
+		JsonNode exp = read("testPrepareBucket");
+		assertEquals(exp, act);
+
 	}
 
 }
