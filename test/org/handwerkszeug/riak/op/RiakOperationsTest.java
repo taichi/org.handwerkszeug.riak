1c3e0e31e215ec9bf5028d481b70a33021632d2d
taichi <ryushi@gmail.com>
2011-06-16 17:57:50 +0900

    add test.

--- test/org/handwerkszeug/riak/op/RiakOperationsTest.java
+++ test/org/handwerkszeug/riak/op/RiakOperationsTest.java
@@ -947,6 +947,66 @@ public abstract class RiakOperationsTest {
 		testDelete(location);
 	}
 
+	@Test
+	public void testFromOfficialsByRawJson() throws Exception {
+		final String bucket = "alice";
+		String p1 = "Alice was beginning to get very tired of sitting by her sister on the"
+				+ " bank, and of having nothing to do: once or twice she had peeped into the"
+				+ " book her sister was reading, but it had no pictures or conversations in"
+				+ " it, 'and what is the use of a book,' thought Alice 'without pictures or"
+				+ " conversation?'";
+		testPut(new Location(bucket, "p1"), p1);
+
+		String p2 = "So she was considering in her own mind (as well as she could, for the"
+				+ " hot day made her feel very sleepy and stupid), whether the pleasure"
+				+ " of making a daisy-chain would be worth the trouble of getting up and"
+				+ " picking the daisies, when suddenly a White Rabbit with pink eyes ran"
+				+ " close by her.";
+		testPut(new Location(bucket, "p2"), p2);
+
+		String p5 = "The rabbit-hole went straight on like a tunnel for some way, and then"
+				+ " dipped suddenly down, so suddenly that Alice had not a moment to think"
+				+ " about stopping herself before she found herself falling down a very deep"
+				+ " well.";
+		testPut(new Location(bucket, "p5"), p5);
+
+		final boolean[] is = { false };
+
+		String query = loadJson(JsonUtil.getJsonPath(RiakOperationsTest.class,
+				"testFromOfficialsByRawJson"));
+
+		final ArrayNode[] actual = new ArrayNode[1];
+		RiakFuture waiter = this.target.mapReduce(query,
+				new RiakResponseHandler<MapReduceResponse>() {
+					@Override
+					public void onError(RiakResponse response) throws Exception {
+						fail(response.getMessage());
+					}
+
+					@Override
+					public void handle(
+							RiakContentsResponse<MapReduceResponse> response)
+							throws Exception {
+						MapReduceResponse mrr = response.getContents();
+						if (mrr.getDone()) {
+							is[0] = true;
+						} else {
+							actual[0] = mrr.getResponse();
+						}
+					}
+				});
+
+		wait(waiter, is);
+
+		testDelete(new Location(bucket, "p1"));
+		testDelete(new Location(bucket, "p2"));
+		testDelete(new Location(bucket, "p5"));
+
+		JsonNode expected = JsonUtil.read(RiakOperationsTest.class,
+				"testFromOfficialsByRawJson_Expected");
+		assertEquals(expected, actual[0]);
+	}
+
 	protected void wait(final RiakFuture waiter, final boolean[] is)
 			throws InterruptedException {
 		assertTrue("test is timeout.", waiter.await(3, TimeUnit.SECONDS));
